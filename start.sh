c15ce22bd5fd0e0d8d9c2cc4b07decf2afed55c4
FeiYehua <31472675+feiyehua@users.noreply.github.com>
2025-01-07 14:57:40 +0800

    Update script

--- start.sh
+++ start.sh
@@ -1,14 +1,15 @@
 ###
  # @Author       : FeiYehua
  # @Date         : 2025-01-06 11:44:25
- # @LastEditTime : 2025-01-06 11:46:49
+ # @LastEditTime : 2025-01-07 14:57:38
  # @LastEditors  : FeiYehua
  # @Description  : 
  # @FilePath     : start.sh
  #      © 2024 FeiYehua
 ### 
 #!/bin/bash
-sudo apt-get install tmux
+apt-get install tmux -y
+mkdir target
 nvcc -rdc=true -O4 main.cu sha1.cu -o target/sha1.out
 tmux
 echo ./target/sha1.out
