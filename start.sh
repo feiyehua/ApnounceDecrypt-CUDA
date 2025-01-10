3620e51496dfcd819424201f8789c822c0cb7285
FeiYehua <31472675+feiyehua@users.noreply.github.com>
2025-01-10 16:48:20 +0800

    Update progress

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 ###
  # @Author       : FeiYehua
  # @Date         : 2025-01-06 11:44:25
- # @LastEditTime : 2025-01-07 14:57:38
+ # @LastEditTime : 2025-01-10 16:48:18
  # @LastEditors  : FeiYehua
  # @Description  : 
  # @FilePath     : start.sh
@@ -12,4 +12,3 @@ apt-get install tmux -y
 mkdir target
 nvcc -rdc=true -O4 main.cu sha1.cu -o target/sha1.out
 tmux
-echo ./target/sha1.out
